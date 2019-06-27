e20c7d7eb12b095a0e0ac744329de08d3fd11a3b
李樟取 <lizhangqu@weidian.com>
2019-06-27 11:54:48 +0800

    * apply patch

--- buildSrc/src/main/groovy/io/github/lizhangqu/tranform/patch/AGPTransformPatchPlugin.groovy
+++ buildSrc/src/main/groovy/io/github/lizhangqu/tranform/patch/AGPTransformPatchPlugin.groovy
@@ -2,12 +2,31 @@ package io.github.lizhangqu.tranform.patch
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.Configuration
+import org.gradle.api.artifacts.Dependency
 
 
 public class AGPTransformPatchPlugin implements Plugin<Project> {
 
     @Override
     final void apply(Project project) {
-        AGPTransformPatch.applyAGPTransformPatch(project, new URL("file:/Users/lizhangqu/.gradle/caches/jars-3/14178045e3ebd34f2544774b98384ccf/android-gradle-plugin-transfrom-patch-1.0.0-20190627.011233-1.jar"))
+        File patchFile
+        try {
+            Dependency dependency = project.getDependencies().create("io.github.lizhangqu:android-gradle-plugin-transfrom-patch:1.0.0")
+            Configuration configuration = project.getConfigurations().detachedConfiguration(dependency)
+            configuration.setTransitive(false)
+            configuration.resolutionStrategy.cacheDynamicVersionsFor(5, 'minutes')
+            configuration.resolutionStrategy.cacheChangingModulesFor(0, 'seconds')
+            patchFile = configuration.getSingleFile()
+        } catch (Exception e) {
+
+        }
+
+        if (patchFile == null) {
+            project.logger.error("can't get patch patchFile")
+            return
+        }
+        AGPTransformPatch.applyAGPTransformPatch(project, file.toURI().toURL())
+
     }
 }
